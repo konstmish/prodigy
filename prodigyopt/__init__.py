ceaf112c8fef4bff6f37a1fbce0f51a8165b2142
Konstantin Mishchenko <konsta.mish@gmail.com>
2023-06-12 02:08:49 +0100

    add pytorch optimizer

---
+++ prodigyopt/__init__.py
@@ -0,0 +1,1 @@
+from .prodigy import Prodigy
---
+++ setup.py
@@ -0,0 +1,25 @@
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="prodigyopt",
+    version="1.0",
+    author="Konstantin Mishchenko",
+    author_email="konsta.mish@gmail.com",
+    description="An Adam-like optimizer for neural networks with adaptive estimation of learning rate",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/konstmish/prodigy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
